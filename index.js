78ee99ddb2dd03555d105930d27abd901893a0f2
root <root@Olimpo>
2024-08-27 15:51:49 -0500

    Cambio de puerto escucha

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/api', (req, res) => {
 });
 
 // Iniciar el servidor
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
